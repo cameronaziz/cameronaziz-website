7200df39ca1d4c135d38c1e29985e5e137df8f2a
Cameron Aziz <cameronaziz@me.com>
2022-02-28 22:27:30 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import { Intro, Skills, Contact, Projects } from 'components/landing';
 
 const Home = () => (
   <Layout>
-<!--     <meta property="og:title" content="Cameron Aziz" />
+    {/*
+    <meta property="og:title" content="Cameron Aziz" />
     <meta property="og:description" content="Web Engineer specializing in React, Typescript and GraphQL." />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://www.cameronaziz.com" />
-    <meta property="og:image" content="/image.jpeg" /> -->
+    <meta property="og:image" content="/image.jpeg" />
+    */}
     <SEO />
     <Intro />
     {/* <Projects /> */}
